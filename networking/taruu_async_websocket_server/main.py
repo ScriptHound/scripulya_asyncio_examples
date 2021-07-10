3e783b6b013e818626130e30cbf6962e417c7c5a
Taruu <markov2002@bk.ru>
2021-07-11 01:01:49 +0300

    [NETWORKING][taruu_async_websocket_server] demo work need refactoring

--- networking/taruu_async_websocket_server/main.py
+++ networking/taruu_async_websocket_server/main.py
@@ -30,9 +30,32 @@ OPCODE_PING = 0x9
 OPCODE_PONG = 0xA
 
 
+def encode_to_UTF8(data):
+    try:
+        return data.encode('UTF-8')
+    except UnicodeEncodeError as e:
+        logger.error("Could not encode data to UTF-8 -- %s" % e)
+        return False
+    except Exception as e:
+        raise (e)
+
+
+def try_decode_UTF8(data):
+    try:
+        return data.decode('utf-8')
+    except UnicodeDecodeError:
+        return False
+    except Exception as e:
+        raise (e)
+
+
 class HandlerData:
     def __iter__(self):
         pass
+
+
+def server_get_message(self, data):
+    logger.info(f"WebSocket get message: {data}")
 
 
 class WebSocketClient:
@@ -42,11 +65,32 @@ class WebSocketClient:
                  writer: asyncio.StreamWriter):
         self.reader = reader
         self.writer = writer
-
+        self.keep_alive = True
+        self.handshake_done = False
+        self.valid_client = False
         pass
 
     async def client_watcher(self):
+        while self.keep_alive:
+            logger.debug(self.handshake_done)
+            if not self.handshake_done:
+                await self.handshake()
+            else:
+                await self.read_next_message()
         pass
+
+    async def relpy_client(self, message):
+        logger.debug(f"Send relpy {message}")
+        message = b"You foxgirl? Okey here you message: " + message.encode()
+        await self.send_text(message)
+
+    async def send_pong(self, message):
+        await self.send_text(message, OPCODE_PONG)
+
+    async def read_bytes(self, num):
+        bytes = await self.reader.read(num)
+        logger.debug(f"bytes {num} {bytes}")
+        return bytes
 
     async def read_http_headers(self):
         """Чтения твоей головы бро"""
@@ -67,15 +111,11 @@ class WebSocketClient:
 
     async def handshake(self):
         headers = await self.read_http_headers()
-        logger.debug(pprint.pformat(headers))
-        logger.debug(headers['upgrade'].lower())
-
         try:
             assert headers['upgrade'].lower() == 'websocket'
         except AssertionError:
             self.keep_alive = False
             return
-
         try:
             key = headers['sec-websocket-key']
         except KeyError:
@@ -86,13 +126,63 @@ class WebSocketClient:
         logger.debug(f"KEY client: {key}")
         response = self.make_handshake_response(key)
         logger.debug(response)
-        self.writer.writelines(response)
+        self.writer.write(response.encode())
         await self.writer.drain()
-        logger.debug(await self.reader.readline())
+        self.handshake_done = True
 
-    def read_next_message(self):
+    async def send_text(self, message, opcode=OPCODE_TEXT):
+        """
+        Important: Fragmented(=continuation) messages are not supported since
+        their usage cases are limited - when we don't know the payload length.
+        """
+
+        # Validate message
+        if isinstance(message, bytes):
+            message = try_decode_UTF8(
+                message)  # this is slower but ensures we have UTF-8
+            if not message:
+                logger.warning(
+                    "Can\'t send message, message is not valid UTF-8")
+                return False
+        else:
+            logger.warning(
+                f'Can\'t send message, message has to be a string or bytes. Given type is {type(message)}')
+            return False
+
+        header = bytearray()
+        payload = encode_to_UTF8(message)
+        payload_length = len(payload)
+
+        # Normal payload
+        if payload_length <= 125:
+            header.append(FIN | opcode)
+            header.append(payload_length)
+
+        # Extended payload
+        elif payload_length >= 126 and payload_length <= 65535:
+            header.append(FIN | opcode)
+            header.append(PAYLOAD_LEN_EXT16)
+            header.extend(struct.pack(">H", payload_length))
+
+        # Huge extended payload
+        elif payload_length < 18446744073709551616:
+            header.append(FIN | opcode)
+            header.append(PAYLOAD_LEN_EXT64)
+            header.extend(struct.pack(">Q", payload_length))
+
+        else:
+            raise Exception(
+                "Message is too big. Consider breaking it into chunks.")
+            return
+
+        logger.debug(f"to send {header + payload}")
+        self.writer.write(header + payload)
+        await self.writer.drain()
+
+    async def read_next_message(self):
+        b1, b2 = await self.read_bytes(2)
         try:
-            b1, b2 = await self.read_bytes(2)
+            pass
         # except SocketError as e:  # to be replaced with ConnectionResetError for py3
         #     if e.errno == errno.ECONNRESET:
         #         logger.info("Client closed connection.")
@@ -101,6 +191,8 @@ class WebSocketClient:
         #     b1, b2 = 0, 0
         except ValueError as e:
             b1, b2 = 0, 0
+
+        logger.debug(f"2 first_bytes {b1, b2}")
 
         fin = b1 & FIN
         opcode = b1 & OPCODE
@@ -112,7 +204,7 @@ class WebSocketClient:
             self.keep_alive = 0
             return
         if not masked:
-            logger.warn("Client must always be masked.")
+            # logger.warn("Client must always be masked.")
             self.keep_alive = 0
             return
         if opcode == OPCODE_CONTINUATION:
@@ -122,41 +214,36 @@ class WebSocketClient:
             logger.warn("Binary frames are not supported.")
             return
         elif opcode == OPCODE_TEXT:
-            opcode_handler = lambda data: logger.info(
-                "get text from client {data}")
+            opcode_handler = self.relpy_client
         elif opcode == OPCODE_PING:
-            opcode_handler = self.server._ping_received_
+            opcode_handler = self.send_pong
         elif opcode == OPCODE_PONG:
-            opcode_handler = self.server._pong_received_
+            opcode_handler = self.send_pong
         else:
             logger.warn("Unknown opcode %#x." % opcode)
             self.keep_alive = 0
             return
 
         if payload_length == 126:
-            payload_length = struct.unpack(">H", self.rfile.read(2))[0]
+            payload_length = struct.unpack(">H", await self.reader.read(2))[0]
         elif payload_length == 127:
-            payload_length = struct.unpack(">Q", self.rfile.read(8))[0]
+            payload_length = struct.unpack(">Q", self.reader.read(8))[0]
 
-        masks = self.read_bytes(4)
+        masks = await self.read_bytes(4)
         message_bytes = bytearray()
         for message_byte in await self.read_bytes(payload_length):
             message_byte ^= masks[len(message_bytes) % 4]
             message_bytes.append(message_byte)
-        opcode_handler(self, message_bytes.decode('utf8'))
 
-    async def read_bytes(self, num):
-        return await self.reader.read(num)
+        logger.debug(message_bytes)
+        await opcode_handler(message_bytes.decode('utf8'))
 
     @classmethod
     def make_handshake_response(cls, key):
-        return list(map(lambda line: line.encode(), [
-            'HTTP/1.1 101 Switching Protocols',
-            'Upgrade: websocket',
-            'Connection: Upgrade',
-            'Sec-WebSocket-Accept: {}'.format(
-                cls.calculate_response_key(key))
-        ]))
+        return f'''HTTP/1.1 101 Switching Protocols\r
+Connection: Upgrade\r
+Sec-WebSocket-Accept: {cls.calculate_response_key(key)}
+Upgrade: websocket\r\n\r\n'''
 
     @classmethod
     def calculate_response_key(cls, key):
@@ -172,7 +259,7 @@ class SimpleWebSocketServer:
 
 
 async def handle_echo(reader, writer):
-    await WebSocketClient(reader, writer).handshake()
+    asyncio.create_task(WebSocketClient(reader, writer).client_watcher())
 
     # data = await reader.read(100)
     # message = data.decode()
@@ -185,7 +272,6 @@ async def handle_echo(reader, writer):
     # await writer.drain()
     #
     # print("Close the connection")
-    writer.close()
 
 
 async def main():
