829a93b2ea9a027dbc4b0d5bf33ff3c686ce95a9
Taruu <markov2002@bk.ru>
2021-08-11 00:10:38 +0300

    [NETWORKING][taruu_socket_select_server] add server_selectors.py

--- networking/taruu_socket_async_server/server_selectors.py
+++ networking/taruu_socket_async_server/server_selectors.py
@@ -1,25 +1,29 @@
 import socket
-from select import select
+import selectors
 import logging
 
 logging.basicConfig(
     format="\033[36m %(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+selector = selectors.DefaultSelector()
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5000))
-server_socket.listen()
 
-connection_to_lookup = [server_socket]
+def server_init():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 5000))
+    server_socket.listen()
+    selector.register(fileobj=server_socket, events=selectors.EVENT_READ,
+                      data=accept_connection)
 
 
 def accept_connection(server_socket):
     logger.info("Accepting connection")
     client_socket, addr = server_socket.accept()
     logger.info(f"Connected form {addr}")
-    connection_to_lookup.append(client_socket)
+    selector.register(fileobj=client_socket, events=selectors.EVENT_READ,
+                      data=send_message)
 
 
 def send_message(client_socket):
@@ -30,19 +34,18 @@ def send_message(client_socket):
         client_socket.send(response)
     else:
         logger.info("Client exit")
+        selector.unregister(client_socket)
         client_socket.close()
-        connection_to_lookup.remove(client_socket)
 
 
 def connection_handler():
-    ready_to_handle, *_ = select(connection_to_lookup, [], [])
-    for sock in ready_to_handle:
-        if sock is server_socket:
-            accept_connection(sock)
-        else:
-            send_message(sock)
+    events = selector.select()
+    for key, _ in events:
+        method = key.data
+        method(key.fileobj)
 
 
 if __name__ == '__main__':
+    server_init()
     while True:
         connection_handler()
